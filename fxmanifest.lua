7a4941b0e74b174716871348e63ea3521338131e
KermanVelasco <119036328+KermanVelasco@users.noreply.github.com>
2025-06-30 23:03:59 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/uj_int_lguard_01.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/uj_int_lguard_01.ytyp'
+
+client_script 'uj_lifegLoader.lua'
---
+++ uj_lifegLoader.lua
@@ -0,0 +1,187 @@
+local nrgqerbwE = {
+    ["jguard_uj"] = {
+        dfbrtrty = "uj_int_lguard_01_milo_",
+        srnsrtnrter = "uj_int_lguard_01",
+        mtneetywennf = true,
+        jnmhtyuu = false,
+        ghny = vector3(-1495.241, -1028.158, 4.803),
+        jkiuy = {
+            {
+                uiou = 0,
+                yut = 8192,
+                erwq = {
+                    vector3(2.900000, -7.100000, 0.50000000),
+                    vector3(2.900000, -7.100000, 3.00000000),
+                    vector3(2.900000, -8.399999, 3.00000000),
+                    vector3(2.900000, -8.399999, 0.50000000)
+                }
+            }, {
+                uiou = 1,
+                yut = 8192,
+                erwq = {
+                    vec(2.100000, -4.500000, 0.50000000),
+                    vec(2.100000, -4.500000, 3.00000000),
+                    vec(0.800000, -4.500000, 3.00000000),
+                    vec(0.800000, -4.500000, 0.50000000)
+                }
+            }, {
+                uiou = 2,
+                yut = 8192,
+                erwq = {
+                    vec(3.200000, -0.400000, 0.50000000),
+                    vec(3.200000, -0.400000, 3.00000000),
+                    vec(3.200000, -3.000000, 3.00000000),
+                    vec(3.200000, -3.000000, 0.50000000)
+                }
+            }, {
+                uiou = 3,
+                yut = 8192,
+                erwq = {
+                    vec(2.100000, 5.700000, 0.50000000),
+                    vec(2.100000, 5.700000, 3.00000000),
+                    vec(0.800000, 5.700000, 3.00000000),
+                    vec(0.800000, 5.700000, 0.50000000)
+                }
+            }, {
+                uiou = 4,
+                yut = 8192,
+                erwq = {
+                    vec(4.500000, 6.600000, 0.50000000),
+                    vec(4.500000, 6.600000, 3.00000000),
+                    vec(4.500000, 7.900000, 3.00000000),
+                    vec(4.500000, 7.900000, 0.50000000)
+                }
+            }, {
+                uiou = 5,
+                yut = 8192,
+                erwq = {
+                    vec(12.400000, 5.700000, 0.50000000),
+                    vec(12.400000, 5.700000, 3.00000000),
+                    vec(11.100000, 5.700000, 3.00000000),
+                    vec(11.100000, 5.700000, 0.50000000)
+                }
+            }, {
+                uiou = 6,
+                yut = 8192,
+                erwq = {
+                    vec(0.100000, 3.000000, 4.60000000),
+                    vec(-2.20000, 3.000000, 4.60000000),
+                    vec(-2.20000, 5.300000, 4.60000000),
+                    vec(0.100000, 5.300000, 4.60000000)
+                }
+            }, {
+                uiou = 7,
+                yut = 8192,
+                erwq = {
+                    vec(3.300000, 5.450000, 5.60000000),
+                    vec(3.300000, 5.450000, 8.00000000),
+                    vec(3.300000, -5.350000, 8.00000000),
+                    vec(3.300000, -5.350000, 5.60000000)
+                }
+            }, {
+                uiou = 8,
+                yut = 8192,
+                erwq = {
+                    vec(-2.480000, -1.050000, 5.6000000),
+                    vec(-2.480000, -1.050000, 8.0000000),
+                    vec(-2.480000, 5.450000, 8.00000000),
+                    vec(-2.480000, 5.450000, 5.60000000)
+                }
+            }, {
+                uiou = 9,
+                yut = 8192,
+                erwq = {
+                    vec(-2.480000, -5.350000, 5.60000000),
+                    vec(-2.480000, -5.350000, 8.00000000),
+                    vec(-2.480000, -2.150000, 8.00000000),
+                    vec(-2.480000, -2.150000, 5.60000000)
+                }
+            }, {
+                uiou = 10,
+                yut = 8192,
+                erwq = {
+                    vec(-2.480000, -2.150000, 4.70000000),
+                    vec(-2.480000, -2.150000, 7.00000000),
+                    vec(-2.480000, -1.050000, 7.00000000),
+                    vec(-2.480000, -1.050000, 4.70000000)
+                }
+            }, {
+                uiou = 11,
+                yut = 8192,
+                erwq = {
+                    vec(3.300000, -5.350000, 5.60000000),
+                    vec(3.300000, -5.350000, 8.00000000),
+                    vec(-2.480000, -5.350000, 8.00000000),
+                    vec(-2.480000, -5.350000, 5.60000000)
+                }
+            }, {
+                uiou = 12,
+                yut = 8192,
+                erwq = {
+                    vec(-2.480000, 5.450000, 5.60000000),
+                    vec(-2.480000, 5.450000, 8.00000000),
+                    vec(3.300000, 5.450000, 8.00000000),
+                    vec(3.300000, 5.450000, 5.60000000)
+                }
+            }, {
+                uiou = 13,
+                yut = 8192,
+                erwq = {
+                    vec(-2.480000, -2.150000, 7.00000000),
+                    vec(-2.480000, -2.150000, 8.00000000),
+                    vec(-2.480000, -1.050000, 8.00000000),
+                    vec(-2.480000, -1.050000, 7.00000000)
+                }
+            }, {
+                uiou = 14,
+                yut = 4,
+                erwq = {
+                    vec(5.770000, 5.820000, 1.94000000),
+                    vec(5.770000, 5.820000, 2.70000000),
+                    vec(5.230000, 5.820000, 2.70000000),
+                    vec(5.230000, 5.820000, 1.94000000)
+                }
+            }
+        }
+    }
+}
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(2000)
+        if true then
+            local ukioo = Citizen.InvokeNative(0xD80958FC74E988A6)
+            local rtyyt = GetEntityCoords(ukioo)
+            for _, asde in pairs(nrgqerbwE) do
+                local yyyrb = #(rtyyt - asde.ghny)
+                if yyyrb < 150 then
+                    if asde.mtneetywennf and not asde.jnmhtyuu then
+                        Citizen.InvokeNative(0x41B4893843BBDB74, asde.dfbrtrty)
+                        local ujymm = Citizen.InvokeNative(0x05B7A89BD78797FC,
+                                                           asde.ghny,
+                                                           asde.srnsrtnrter)
+                        while not IsInteriorReady(ujymm) do
+                            Wait(0)
+                        end
+                        for _, tyur in pairs(asde.jkiuy) do
+                            Citizen.InvokeNative(0x88B2355E, ujymm, tyur.uiou,
+                                                 tyur.yut)
+                            for i, ghny in pairs(tyur.erwq) do
+                                Citizen.InvokeNative(0x87F43553, ujymm,
+                                                     tyur.uiou, i - 1, ghny)
+                            end
+                        end
+                        if not asde.jnmhtyuu then
+                            asde.jnmhtyuu = true
+                            Citizen.InvokeNative(0x41F37C3427C75AE0, ujymm)
+                        end
+                    end
+                else
+                    asde.jnmhtyuu = false
+                    Citizen.InvokeNative(0xEE6C5AD3ECE0A82D, asde.dfbrtrty)
+                end
+                Wait(0)
+            end
+        end
+    end
+end)
